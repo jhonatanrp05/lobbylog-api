b863f6ebc1be9164125ed119ae1171a833a78302
jhonatanrp05 <jpachecos@uninorte.edu.co>
2026-05-18 11:30:46 -0500

    feat: implement soft delete for users by adding deletedAt field

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -8,34 +8,34 @@ export const createUser = async (data: User) => {
 };
 
 export const findUserByEmail = async (email: string) => {
-  return await prisma.user.findUnique({
-    where: {
-      email: email,
-    },
+  return await prisma.user.findFirst({
+    where: { email, deletedAt: null },
   });
 };
 
 export const findAllUsers = async () => {
   return await prisma.user.findMany({
+    where: { deletedAt: null },
     select: { id: true, name: true, email: true, role: true, unit: true },
   });
 };
 
 export const findUserById = async (id: string) => {
-  return await prisma.user.findUnique({
-    where: { id: id },
+  return await prisma.user.findFirst({
+    where: { id, deletedAt: null },
   });
 };
 
 export const deleteUserById = async (id: string) => {
-  return await prisma.user.delete({
-    where: { id: id },
+  return await prisma.user.update({
+    where: { id },
+    data: { deletedAt: new Date() },
   });
 };
 
 export const findAllResidents = async () => {
   return await prisma.user.findMany({
-    where: { role: "RESIDENT" },
+    where: { role: "RESIDENT", deletedAt: null },
     select: { id: true, name: true, unit: true },
   });
 };
