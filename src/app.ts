a8242956725c786af39ef8bdcdc98f1d99d9432c
jhonatanrp05 <jpachecos@uninorte.edu.co>
2026-05-17 22:12:10 -0500

    feat: add cors support to recieve client requests

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 import packageRoutes from "./routes/package.routes";
 
 const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
